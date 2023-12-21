1e47e3b7cfd758ece7077e43015adf1967cef164
Ayush W <113709655+AshWatts@users.noreply.github.com>
2023-12-22 00:02:01 +0530

    `

--- Command_Line_Arguments_1.cpp
+++ Command_Line_Arguments_1.cpp
@@ -1,43 +1,43 @@
-// // program named mainreturn.cpp
-// #include <iostream>
-// using namespace std;
-
-// // defining main with arguments
-// int main(int argc, char* argv[])
-// {
-// 	cout << "You have entered " << argc << " arguments:"<< "\n";
-
-// 	for (int i = 0; i < argc; ++i)
-// 		cout << argv[i] << "\n";
-
-// 	return 0;
-// }
-
-
-// C++ program to illustrate command line arguments
+// program named mainreturn.cpp
 #include <iostream>
 using namespace std;
 
+// defining main with arguments
 int main(int argc, char* argv[])
 {
-	cout << "Program name is: " << argv[0] << endl;
+	cout << "You have entered " << argc << " arguments:"<< "\n";
 
-	if (argc == 1) 
-    {
-		cout << "No extra Command Line Argument passed "
-				"other than program name"
-			<< endl;
-	}
-	if (argc > 1) 
-    {
-		cout << "Number of arguments passed: " << argc
-			<< endl;
-		cout << "----Following are the commnand line "
-				"arguments passed----"
-			<< endl;
-		for (int i = 0; i < argc; i++) {
-			cout << "argv[" << i << "]: " << argv[i]
-				<< '\n';
-		}
-	}
+	for (int i = 0; i < argc; ++i)
+		cout << argv[i] << "\n";
+
+	return 0;
 }
+
+
+// // C++ program to illustrate command line arguments
+// #include <iostream>
+// using namespace std;
+
+// int main(int argc, char* argv[])
+// {
+// 	cout << "Program name is: " << argv[0] << endl;
+
+// 	if (argc == 1) 
+//     {
+// 		cout << "No extra Command Line Argument passed "
+// 				"other than program name"
+// 			<< endl;
+// 	}
+// 	if (argc > 1) 
+//     {
+// 		cout << "Number of arguments passed: " << argc
+// 			<< endl;
+// 		cout << "----Following are the commnand line "
+// 				"arguments passed----"
+// 			<< endl;
+// 		for (int i = 0; i < argc; i++) {
+// 			cout << "argv[" << i << "]: " << argv[i]
+// 				<< '\n';
+// 		}
+// 	}
+// }
